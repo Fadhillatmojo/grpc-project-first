6809dadbd70c9155cf54521a4e266c33065d5cdc
User <fadhillatmojo@gmail.com>
2024-09-06 21:47:43 +0700

    bidirectional streaming data flow

---
+++ client/bi_stream.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/fadhillatmojo/grpc-project-first/proto"
+)
+
+func callHelloBiderectionalStream(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Printf("Bidirectional Streaming Started")
+	stream, err := client.SayHelloBiderectionalStreaming(context.Background())
+	if err != nil {
+		log.Fatalf("could not send names: %v", err)
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for {
+			message, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while streaming: %v", err)
+			}
+
+			log.Println(message)
+		}
+		close(waitc)
+	}()
+
+	for _, name := range names.Names {
+		req := &pb.HelloRequest{
+			Name: name,
+		}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("error while sending %v", err)
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+
+	stream.CloseSend()
+	<-waitc
+	log.Printf("Bidirectional Streaming finished")
+}
